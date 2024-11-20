a5f00e83f524da2db8fcb4aca23f7972b1238b50
amateur5 <122245513+amateur5@users.noreply.github.com>
2024-11-20 08:28:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ io.on('connection', (socket) => {
     });
 });
 
-// Pokretanje servera
-server.listen(3000, () => {
-    console.log('Server pokrenut na http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server je pokrenut na portu ${PORT}`);
 });
